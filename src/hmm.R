1f0e9a5f5fb67ac19ef970d25e6f3d167dd4d0f8
Lewis Marsh <lewismarsh@Lewiss-MacBook-Air.local>
2018-06-13 16:19:59 +0300

    Added plotting features and fixed bug in hmm.R, updated the .rmd file

--- src/hmm.R
+++ src/hmm.R
@@ -3,7 +3,7 @@ library(quantmod)
 library(ggplot2)
 
 
-create_hmm_states_student <- function(data, globalid, max_iterations = 3){
+create_hmm_states_student <- function(data, globalid, max_states = 3){
   #Selects data of the student passed on to the function
   student_data <- subset(new_data, global.id == globalid)
 
@@ -39,11 +39,18 @@ create_hmm_states_student <- function(data, globalid, max_iterations = 3){
   
   loglik1 <- as.data.frame(logLik(HMMfit, method=c("fb","lystig","classification"),na.allow=TRUE))[1,1]
   
-  for(i in 2:max_iterations){
+  measures <- data.frame(1, loglik1, AIC(HMMfit), BIC(HMMfit))
+  names(measures) <- c("nstates", "logLik", "AIC", "BIC")
+  
+  for(i in 2:max_states){
     HMM2 <- depmix(response = responses, data = TSData, nstates=i, family = family)
     HMMfit2 <- fit(HMM2, verbose = FALSE, emc=em.control(rand=T))
     
     loglik2 <- as.data.frame(logLik(HMMfit2, method=c("fb","lystig","classification"),na.allow=TRUE))[1,1]
+    
+    measures2 <- data.frame(1, loglik2, AIC(HMMfit2), BIC(HMMfit2))
+    names(measures2) <- c("nstates", "logLik", "AIC", "BIC")
+    measures <- rbind(measures, measures2)
     
     if(loglik2 > loglik1){
       HMM <- HMM2
@@ -66,7 +73,7 @@ remove_unique_students <- function(data){
 }
 
 
-create_hmm_states_global <- function(data, max_iterations = 3, sorted = F){
+create_hmm_states_global <- function(data, max_states = 3, sorted = F, visualise = T){
   previous_id <- " "
   counter <- 0
   indicator <- c()
@@ -97,18 +104,23 @@ create_hmm_states_global <- function(data, max_iterations = 3, sorted = F){
                  multinomial(),multinomial(),multinomial())
   responses <- c(disengaged ~ 1,looking ~ 1,talking ~ 1,technology ~ 1,resources ~ 1,external ~ 1)
   
-  print(indicator)
-
   HMM <- depmix(response = responses, data = hmm_data, nstates=1, family = family, ntimes = indicator)
-  HMMfit <- fit(HMM, verbose = FALSE, emc=em.control(rand=T))
+  HMMfit <- fit(HMM, verbose = FALSE, emc=em.control(random.start = FALSE))
   
   loglik1 <- as.data.frame(logLik(HMMfit, method=c("fb","lystig","classification"),na.allow=TRUE))[1,1]
   
-  for(i in 2:max_iterations){
+  measures <- data.frame(1, loglik1, AIC(HMMfit), BIC(HMMfit))
+  names(measures) <- c("nstates", "logLik", "AIC", "BIC")
+  
+  for(i in 2:max_states){
     HMM2 <- depmix(response = responses, data = data, nstates=i, family = family, ntimes = indicator)
-    HMMfit2 <- fit(HMM2, verbose = FALSE, emc=em.control(rand=T))
+    HMMfit2 <- fit(HMM2, verbose = FALSE, emc=em.control(random.start = TRUE))
     
     loglik2 <- as.data.frame(logLik(HMMfit2, method=c("fb","lystig","classification"),na.allow=TRUE))[1,1]
+    
+    measures2 <- data.frame(i, loglik2, AIC(HMMfit2), BIC(HMMfit2))
+    names(measures2) <- c("nstates", "logLik", "AIC", "BIC")
+    measures <- rbind(measures, measures2)
     
     if(loglik2 > loglik1){
       HMM <- HMM2
@@ -117,7 +129,24 @@ create_hmm_states_global <- function(data, max_iterations = 3, sorted = F){
     }
   }
   
+  if(visualise){
+      par(mfrow=c(2,1))
+      plot(measures$nstates, measures$AIC, main="Minimise (Cirlces = AIC, Diamonds = BIC)")
+      #legend(1, 1, legend=c("AIC", "BIC"), pch = c(19, 23), cex=0.8)
+      points(measures$nstates, measures$BIC, pch = 23)
+      plot(measures$nstates, measures$logLik, main="Maximise", pch = 22)
+  }
+  
   HMMfit
+}
+
+## Just auxillary function to keep knitting time of .Rmd file at reasonalbe levels
+plot_criteria <- function(measures){
+  par(mfrow=c(2,1))
+  plot(measures$nstates, measures$AIC, main="Minimise (Cirlces = AIC, Diamonds = BIC)")
+  points(measures$nstates, measures$BIC, pch = 23)
+  plot(measures$nstates, measures$logLik, main="Maximise", pch = 22)
+  legend(1, 1, legend=c("AIC", "BIC"), pch = c(19, 23))
 }
 
 
@@ -153,35 +182,38 @@ get_response_list <- function(input){
   responses
 }
 
-### Will create better plotting function tomorrow
-compare_with_mca <- function(HMMfit, MCA){
-  datetime <- as.character(MCA$timestamp)
-  datetime <- as.POSIXlt(datetime, format = "%d/%m/%Y %H:%M:%S")
-  MCA$timestamp <- datetime
-  
-  HMMfit <- posterior(HMMfit)
-  HMMfit$timestamp <- datetime
-  
-  p <- ggplot() + geom_smooth(data = MCA, aes(x = timestamp, y = MCAdim1),
-                              colour="darkblue", size=1) +
-                geom_smooth(data = HMMfit, aes(x = timestamp, y = S1), fill="red", colour="red", size=1)
-  
-  print(p)
-}
 
-
-insert_hmm<- function(data){
+insert_hmm <- function(data, initial_state = 1){
   data <- remove_unique_students(data)
   data <- data[order(as.character(data$global.id)), ]
   
-  HMMfit <- create_hmm_states_global(data, sorted = T)
+  #set.seed(initial_state)
+  
+  HMMfit <- create_hmm_states_global(data, sorted = TRUE, visualise = FALSE)
   post <- posterior(HMMfit)
   
+  data$HMMPredS <- post$state
   data$HMMS1 <- post$S1
   data$HMMS2 <- post$S2
   data$HMMS3 <- post$S3
   
   data
+}
+
+
+plot_development_by_student <- function(data, globalid){
+  student_data <- subset(data, global.id == globalid)
+  
+  datetime <- as.character(student_data[,1])
+  datetime <- as.POSIXlt(datetime, format = "%d/%m/%Y %H:%M:%S")
+  
+  student_data$timestamp <- datetime
+  
+  p <- ggplot() + geom_smooth(data = student_data, aes(x = timestamp, y = MCAdim1),
+                              colour="darkblue", size=1) +
+        geom_smooth(data = student_data, aes(x = timestamp, y = HMMPredS), fill="red", colour="red", size=1)
+  
+  print(p)
 }
 
 
